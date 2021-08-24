96ad75b9a6c28800b1f8cd824af924e122d8e025
realshegz <abiodun_segun10@yahoo.com>
2021-08-24 11:21:30 +0100

    modified translator assignment

--- assignment/assignment.js
+++
@@ -1,6 +0,0 @@
-//Create a function that accept multiple arrays as arguments and returns unique array
-function uniqueArr(){
-    let value = [...arguments].flat()
-    let uniqueValue = [...new Set(value)]
-    return uniqueValue
-}git
